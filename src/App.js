783a72ea8d991df82d8a875532bc723d40060bbf
Maurice Muthaka <Maurice@macs-MacBook-Air-144.local>
2020-04-22 23:55:51 +0300

    My first App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import styles from './App.module.css';
+import { fetchData } from './api';
+import covid from './images/cov.png';
+import me from './images/me.jpeg';
+import Avatar from '@material-ui/core/Avatar';
+import Fab from '@material-ui/core/Fab';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Cards, Chart, CountryPicker } from './components';
+
+class App extends Component {
+
+    state = {
+        data:{},
+        country: '',
+    }
+
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+
+        this.setState({ data: fetchedData });
+    }
+
+    handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country);
+        this.setState({ data: fetchedData, country: country });
+    }
+
+    portfolio(){
+        const url = 'https://mauricemuthaka.co.ke';
+        window.open(url, '_blank');
+    }
+
+    render() { 
+        const { data, country } = this.state;
+
+        return (
+            <div className={styles.container}>
+                <div className={styles.header}>
+                    <img src={covid} className={styles.img} alt="COVID 19" />
+                    <div className={styles.title}>
+                        <h2 className={styles.h2}>COVID-19 LIVE TRACKER</h2>
+                        <span className={styles.name}>developed by Maurice Muthaka</span>
+                    </div>
+                </div>
+                <CountryPicker handleCountryChange ={this.handleCountryChange} />
+                <Cards data={data} />
+                <Chart data={data} country={country} />
+                <Fab onClick={this.portfolio} color="primary" variant="extended" className={styles.menu} aria-label="add">
+                    {<Avatar className={styles.avatar} alt="Mm" src={me} />}
+                    My Portfolio
+                </Fab>
+
+            </div>
+        );
+    }
 }
-
+ 
 export default App;
